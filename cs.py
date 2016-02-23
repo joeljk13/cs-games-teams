75c94a80a233dfd12ec81b3f0dc67de766a583f1
Joel Kottas <sumnon@cj3.org>
2016-02-22 21:47:11 -0500

    Update again

--- cs.py
+++ cs.py
@@ -6,6 +6,7 @@ import itertools
 
 def main():
     people = [
+        "JK",
         "CW",
         "JA",
         "MJ",
@@ -23,10 +24,10 @@ def main():
         "CP",
         "AC",
         "JD",
-        "JK",
         "LK",
         "JB"
     ]
+
     comps = {
         'alg': ["JA", "DR", "MJ", "ZP"],
         'extreme': ["NB", "DH", "LK", "SM"],
@@ -37,12 +38,13 @@ def main():
         'parallel': ["CP", "JK", "JA", "JB"],
         'theory': ["AS", "LK", "DR", "SM"],
         'debug': ["GM", "CP", "JK", "DR"],
-        'soft': ["MK", "JA", "SM", "JJ"], # maybe MK instead of JJ
+        'soft': ["MK", "JA", "SM", "JJ"],
         'mobile': ["NB", "CW", "DH", "NC"],
-        'sg': ["AS", "CW", "JD", "NC"],
+        # 'sg': ["AS", "CW", "JD", "NC"],
         'rev': ["CW", "JK", "CP", "DR"],
         'comp': ["MJ", "JB", "AC", "LK"]
     }
+
     for person in people:
         s = ""
         c = 0
@@ -51,6 +53,7 @@ def main():
                 s += " " + x
                 c += 1
         print person + " " + str(c + (person in comps['ai'])) + s
+
     def test_team(team1):
         team2 = list(set(range(20)) - set(team1))
         team1 = map(lambda x: people[x], team1)
@@ -63,11 +66,15 @@ def main():
             if abs(len([p for p in players if p in team1]) - len(players) / 2) != 0:
                 cp.append(c)
             # assert len([p for p in players if p in team2]) == len(players) / 2
+        team1.sort()
+        team2.sort()
         return team1, team2, conflicts, cp
+
     L = map(lambda x:
         test_team(x), itertools.combinations(range(20), 10))
     L = filter(lambda x: people[0] in x[0], L)
     L.sort(key=lambda x: x[2])
     pprint(list(itertools.takewhile(lambda x: x[2] <= 1, L)))
+
 if __name__ == "__main__":
     main()
