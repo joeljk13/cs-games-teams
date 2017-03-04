0954cd904554158fce64f414560f7246dda0d163
Joel Kottas <sumnon@cj3.org>
2017-03-04 13:58:23 -0500

    Update - not generating feasible solution

--- cs.py
+++ cs.py
@@ -5,399 +5,108 @@
 # import itertools
 
 PEOPLE = [
-    {
-        'name': 'Antonio',
-        'ai': 1,
-        'relay': 9,
-        'SE': 2,
-        'extreme': 3,
-        'theory': 7,
-        'sport': 11,
-        'gaming': 10,
-        'RE': 6,
-        'security': 4,
-        'OS': 5,
-        'mystery': 8,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Ben',
-        'ai': 4,
-        'relay': 3,
-        'SE': 1,
-        'extreme': 2,
-        'theory': -1,
-        'sport': -1,
-        'gaming': 7,
-        'RE': 2,
-        'security': 6,
-        'OS': 1,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Brandon',
-        'ai': 1,
-        'relay': 4,
-        'SE': 2,
-        'extreme': 2,
-        'theory': 4,
-        'sport': 8,
-        'gaming': 8,
-        'RE': 16,
-        'security': 16,
-        'OS': 8,
-        'mystery': 6,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Colin',
-        'ai': 5,
-        'relay': 4,
-        'SE': -1,
-        'extreme': 3,
-        'theory': 2,
-        'sport': -1,
-        'gaming': 3,
-        'RE': 1,
-        'security': 1,
-        'OS': 1,
-        'mystery': 2,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Dan',
-        'ai': -1,
-        'relay': -1,
-        'SE': -1,
-        'extreme': 6,
-        'theory': 1,
-        'sport': 6,
-        'gaming': -1,
-        'RE': 2,
-        'security': 1,
-        'OS': -1,
-        'mystery': 6,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Gayeon',
-        'ai': 3,
-        'relay': 1,
-        'SE': 3,
-        'extreme': 1,
-        'theory': -1,
-        'sport': 5,
-        'gaming': 6,
-        'RE': -1,
-        'security': -1,
-        'OS': -1,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Grace',
-        'ai': 5,
-        'relay': 3,
-        'SE': 3,
-        'extreme': 5,
-        'theory': 10,
-        'sport': 2,
-        'gaming': 5,
-        'RE': 1,
-        'security': 1,
-        'OS': 5,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Hassler',
-        'ai': 2,
-        'relay': 1,
-        'SE': -1,
-        'extreme': 5,
-        'theory': 6,
-        'sport': 3,
-        'gaming': -1,
-        'RE': -1,
-        'security': -1,
-        'OS': 5,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Jack',
-        'ai': 2,
-        'relay': 3,
-        'SE': 5,
-        'extreme': 4,
-        'theory': 11,
-        'sport': 6,
-        'gaming': 10,
-        'RE': 7,
-        'security': 9,
-        'OS': 8,
-        'mystery': 1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Jackson',
-        'ai': 2,
-        'relay': -1,
-        'SE': 4,
-        'extreme': 3,
-        'theory': 1,
-        'sport': -1,
-        'gaming': 5,
-        'RE': -1,
-        'security': -1,
-        'OS': -1,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Jacob',
-        'ai': 2,
-        'relay': 1,
-        'SE': 2,
-        'extreme': 2,
-        'theory': 1000000,
-        'sport': 3,
-        'gaming': 2,
-        'RE': 4,
-        'security': 1,
-        'OS': 4,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Joel',
-        'ai': 2,
-        'relay': 4,
-        'SE': 10,
-        'extreme': 5,
-        'theory': 5,
-        'sport': -1,
-        'gaming': -1,
-        'RE': 2,
-        'security': 2,
-        'OS': 2,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Johnny',
-        'ai': 1,
-        'relay': 4,
-        'SE': 1,
-        'extreme': 5,
-        'theory': -1,
-        'sport': 3,
-        'gaming': 3,
-        'RE': 20,
-        'security': 20,
-        'OS': 20,
-        'mystery': 6,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Lia',
-        'ai': 4,
-        'relay': 5,
-        'SE': 3,
-        'extreme': 1,
-        'theory': 2,
-        'sport': 6,
-        'gaming': -1,
-        'RE': -1,
-        'security': -1,
-        'OS': -1,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Maria',
-        'ai': -1,
-        'relay': -1,
-        'SE': -1,
-        'extreme': 3,
-        'theory': 1,
-        'sport': 7,
-        'gaming': -1,
-        'RE': -1,
-        'security': 6,
-        'OS': 1000000,
-        'mystery': 7,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Mikayla',
-        'ai': 4,
-        'relay': 5,
-        'SE': 3,
-        'extreme': 1,
-        'theory': 2,
-        'sport': -1,
-        'gaming': 6,
-        'RE': -1,
-        'security': -1,
-        'OS': -1,
-        'mystery': -1,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Nathan',
-        'ai': 1,
-        'relay': 8,
-        'SE': 2,
-        'extreme': 3,
-        'theory': 3,
-        'sport': 7,
-        'gaming': 7,
-        'RE': 5,
-        'security': 4,
-        'OS': 4,
-        'mystery': 1000000,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Ned',
-        'ai': 3,
-        'relay': 4,
-        'SE': 5,
-        'extreme': 6,
-        'theory': 7,
-        'sport': 8,
-        'gaming': 9,
-        'RE': 2,
-        'security': 10,
-        'OS': 1,
-        'mystery': 11,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Shir',
-        'ai': 3,
-        'relay': 4,
-        'SE': 5,
-        'extreme': 2,
-        'theory': 1,
-        'sport': -1,
-        'gaming': -1,
-        'RE': -1,
-        'security': -1,
-        'OS': -1,
-        'mystery': 6,
-        'ALL': 0
-    },
-
-    {
-        'name': 'Zach',
-        'ai': 6,
-        'relay': 2,
-        'SE': 7,
-        'extreme': 1,
-        'theory': 4,
-        'sport': -1,
-        'gaming': 3,
-        'RE': -1,
-        'security': -1,
-        'OS': -1,
-        'mystery': 5,
-        'ALL': 0
-    }
+{'name': 'Antonio', 'ai': 1, 'RE': 6, 'SE': 2, 'CSEC': 1000, 'extreme': 3, 'relay': 9, 'security': 4, 'theory': 4, 'sport': 7, 'web': 11, 'os': 1000, 'debugging': 5, 'mobile': 1000, 'gaming': 1000, 'mystery': 10, 'ALL': 0},
+{'name': 'Ben', 'ai': 4, 'RE': 2, 'SE': 2, 'CSEC': 1000, 'extreme': 2, 'relay': 4, 'security': 6, 'theory': 6, 'sport': 1000, 'web': 1000, 'os': 1000, 'debugging': 1, 'mobile': 1, 'gaming': 1000, 'mystery': 7, 'ALL': 0},
+{'name': 'Brandon', 'ai': 1, 'RE': 16, 'SE': 2, 'CSEC': 1000, 'extreme': 2, 'relay': 4, 'security': 16, 'theory': 16, 'sport': 4, 'web': 8, 'os': 1000, 'debugging': 8, 'mobile': 1000, 'gaming': 1000, 'mystery': 8, 'ALL': 0},
+{'name': 'Colin', 'ai': 10, 'RE': 2, 'SE': 1000, 'CSEC': 1, 'extreme': 5, 'relay': 8, 'security': 1, 'theory': 1, 'sport': 3, 'web': 1000, 'os': 1000, 'debugging': 3, 'mobile': 4, 'gaming': 1000, 'mystery': 5, 'ALL': 0},
+{'name': 'Dan', 'ai': 1000, 'RE': 2, 'SE': 1000, 'CSEC': 4, 'extreme': 6, 'relay': 1000, 'security': 1, 'theory': 1, 'sport': 1, 'web': 6, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
+{'name': 'Gayeon', 'ai': 5, 'RE': 1000, 'SE': 4, 'CSEC': 1000, 'extreme': 1, 'relay': 2, 'security': 1000, 'theory': 1000, 'sport': 1000, 'web': 2, 'os': 1000, 'debugging': 1000, 'mobile': 6, 'gaming': 6, 'mystery': 6, 'ALL': 0},
+{'name': 'Grace', 'ai': 7, 'RE': 1, 'SE': 3, 'CSEC': 2, 'extreme': 5, 'relay': 3, 'security': 1, 'theory': 1, 'sport': 10, 'web': 2, 'os': 6, 'debugging': 5, 'mobile': 5, 'gaming': 6, 'mystery': 5, 'ALL': 0},
+{'name': 'Hassler', 'ai': 2, 'RE': 1000, 'SE': 1000, 'CSEC': 1000, 'extreme': 5, 'relay': 1, 'security': 1000, 'theory': 1000, 'sport': 6, 'web': 3, 'os': 1000, 'debugging': 5, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
+{'name': 'Jack', 'ai': 0, 'RE': 2, 'SE': 9, 'CSEC': 6, 'extreme': 11, 'relay': 4, 'security': 3, 'theory': 3, 'sport': 10, 'web': 13, 'os': 7, 'debugging': 5, 'mobile': 12, 'gaming': 10, 'mystery': 8, 'ALL': 0},
+{'name': 'Jackson', 'ai': 2, 'RE': 1000, 'SE': 4, 'CSEC': 1000, 'extreme': 3, 'relay': 1000, 'security': 1000, 'theory': 1000, 'sport': 1, 'web': 1000, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 1000, 'mystery': 5, 'ALL': 0},
+{'name': 'Jacob', 'ai': 2, 'RE': 4, 'SE': 2, 'CSEC': 1, 'extreme': 1, 'relay': 1, 'security': 1, 'theory': 1, 'sport': 1000, 'web': 3, 'os': 3, 'debugging': 4, 'mobile': 2, 'gaming': 5, 'mystery': 2, 'ALL': 0},
+{'name': 'Joel', 'ai': 2, 'RE': 2, 'SE': 10, 'CSEC': 1000, 'extreme': 5, 'relay': 4, 'security': 2, 'theory': 2, 'sport': 5, 'web': 1000, 'os': 1000, 'debugging': 2, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
+{'name': 'Johnny', 'ai': 1, 'RE': 20, 'SE': 1, 'CSEC': 1000, 'extreme': 5, 'relay': 4, 'security': 20, 'theory': 20, 'sport': 1000, 'web': 3, 'os': 1000, 'debugging': 20, 'mobile': 1000, 'gaming': 1000, 'mystery': 3, 'ALL': 0},
+{'name': 'Lia', 'ai': 5, 'RE': 20, 'SE': 6, 'CSEC': 1000, 'extreme': 1, 'relay': 6, 'security': 1000, 'theory': 1000, 'sport': 5, 'web': 8, 'os': 1000, 'debugging': 1000, 'mobile': 6, 'gaming': 3, 'mystery': 1000, 'ALL': 0},
+{'name': 'Maria', 'ai': 1000, 'RE': 1000, 'SE': 1000, 'CSEC': 1000, 'extreme': 3, 'relay': 1000, 'security': 6, 'theory': 6, 'sport': 1, 'web': 7, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
+{'name': 'Mikayla', 'ai': 5, 'RE': 1000, 'SE': 3, 'CSEC': 1000, 'extreme': 1, 'relay': 6, 'security': 1000, 'theory': 1000, 'sport': 2, 'web': 1000, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 4, 'mystery': 7, 'ALL': 0},
+{'name': 'Nathan', 'ai': 1, 'RE': 5, 'SE': 2, 'CSEC': 1000, 'extreme': 3, 'relay': 8, 'security': 4, 'theory': 4, 'sport': 3, 'web': 7, 'os': 1000, 'debugging': 4, 'mobile': 1000, 'gaming': 1000, 'mystery': 7, 'ALL': 0},
+{'name': 'Ned', 'ai': 3, 'RE': 2, 'SE': 5, 'CSEC': 1000, 'extreme': 6, 'relay': 4, 'security': 10, 'theory': 10, 'sport': 7, 'web': 8, 'os': 1000, 'debugging': 1, 'mobile': 1000, 'gaming': 1000, 'mystery': 9, 'ALL': 0},
+{'name': 'Shir', 'ai': 3, 'RE': 1000, 'SE': 5, 'CSEC': 1000, 'extreme': 2, 'relay': 4, 'security': 1000, 'theory': 1000, 'sport': 1, 'web': 1000, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
+{'name': 'Zach', 'ai': 6, 'RE': 7, 'SE': 1000, 'CSEC': 1, 'extreme': 2, 'relay': 1000, 'security': 3, 'theory': 3, 'sport': 1000, 'web': 1000, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 4, 'mystery': 5, 'ALL': 0},
 ]
 
 CATEGORIES = [
     {
         'name': 'ai',
         'people': 3,
-        'slots': 1,
-        'time': 1
+        'time': [1, 2]
     },
     {
-        'name': 'relay',
+        'name': 'RE',
         'people': 3,
-        'slots': 1,
-        'time': 2
+        'time': [1]
     },
     {
         'name': 'SE',
         'people': 3,
-        'slots': 1,
-        'time': 3
+        'time': [1]
+    },
+    {
+        'name': 'CSEC',
+        'people': 2,
+        'time': [2]
     },
     {
         'name': 'extreme',
         'people': 2,
-        'slots': 1,
-        'time': 5
+        'time': [2]
     },
     {
-        'name': 'theory',
-        'people': 2,
-        'slots': 1,
-        'time': 3
-    },
-    {
-        'name': 'sport',
-        'people': 2,
-        'slots': 1,
-        'time': 5
-    },
-    {
-        'name': 'gaming',
-        'people': 2,
-        'slots': 1,
-        'time': 4
-    },
-    {
-        'name': 'RE',
-        'people': 2,
-        'slots': 1,
-        'time': 1
+        'name': 'relay',
+        'people': 3,
+        'time': [3]
     },
     {
         'name': 'security',
         'people': 2,
-        'slots': 1,
-        'time': 4
+        'time': [3]
     },
     {
-        'name': 'OS',
+        'name': 'theory',
         'people': 2,
-        'slots': 1,
-        'time': 2
+        'time': [3]
+    },
+    {
+        'name': 'sport',
+        'people': 3,
+        'time': [4]
+    },
+    {
+        'name': 'web',
+        'people': 2,
+        'time': [4],
+    },
+    {
+        'name': 'os',
+        'people': 2,
+        'time': [4],
+    },
+    {
+        'name': 'debugging',
+        'people': 2,
+        'time': [5],
+    },
+    {
+        'name': 'mobile',
+        'people': 2,
+        'time': [5],
+    },
+    {
+        'name': 'gaming',
+        'people': 2,
+        'time': [5],
     },
     {
         'name': 'mystery',
         'people': 2,
-        'slots': 1,
-        'time': 5
+        'time': [6],
     },
     {
         'name': 'ALL',
         'people': 10,
-        'slots': 1,
-        'time': 0
+        'time': [0],
     }
 ]
 
@@ -405,7 +114,7 @@ N_PEOPLE = len(PEOPLE)
 N_CATEGORIES = len(CATEGORIES)
 N_TEAMS = 2
 
-MIN_CATS = 3
+MIN_CATS = 2
 
 # ILP Formation
 #
@@ -495,8 +204,8 @@ def main():
     for p in range(N_PEOPLE):
         eq = [0] * N_VARS
         for c in range(N_CATEGORIES):
-            eq[get_var(p, 0, c)] = 1
-            eq[get_var(p, 1, c)] = 1
+            eq[get_var(p, 0, c)] = len(CATEGORIES[c]['time'])
+            eq[get_var(p, 1, c)] = len(CATEGORIES[c]['time'])
         s += str_eq(eq, "G", MIN_CATS)
 
     # For each c, t: SUM_{p} x_{p, t, c} = people_{c}
@@ -512,8 +221,15 @@ def main():
     for p in range(N_PEOPLE):
         for c1 in range(N_CATEGORIES):
             for c2 in range(N_CATEGORIES):
-                if c1 == c2 or \
-                        CATEGORIES[c1]['time'] != CATEGORIES[c2]['time']:
+                if c1 == c2:
+                    continue
+                cont = False
+                for a in CATEGORIES[c1]['time']:
+                    for b in CATEGORIES[c2]['time']:
+                        if a == b:
+                            cont = True
+
+                if cont:
                     continue
 
                 eq = [0] * N_VARS
