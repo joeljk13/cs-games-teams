fc64176ae8b5445275acfa20305cbc2326aa203d
Joel Kottas <sumnon@cj3.org>
2018-02-22 09:54:57 -0500

    Attempt to update for 2018

--- cs.py
+++ cs.py
@@ -5,53 +5,38 @@
 # import itertools
 
 PEOPLE = [
-{'name': 'Antonio', 'ai': 1, 'RE': 6, 'SE': 2, 'CSEC': 1000, 'extreme': 3, 'relay': 9, 'security': 4, 'theory': 4, 'sport': 7, 'web': 11, 'os': 1000, 'debugging': 5, 'mobile': 1000, 'gaming': 1000, 'mystery': 10, 'ALL': 0},
-{'name': 'Ben', 'ai': 4, 'RE': 2, 'SE': 2, 'CSEC': 1000, 'extreme': 2, 'relay': 4, 'security': 6, 'theory': 6, 'sport': 1000, 'web': 1000, 'os': 1000, 'debugging': 1, 'mobile': 1, 'gaming': 1000, 'mystery': 7, 'ALL': 0},
-{'name': 'Brandon', 'ai': 1, 'RE': 16, 'SE': 2, 'CSEC': 1000, 'extreme': 2, 'relay': 4, 'security': 16, 'theory': 16, 'sport': 4, 'web': 8, 'os': 1000, 'debugging': 8, 'mobile': 1000, 'gaming': 1000, 'mystery': 8, 'ALL': 0},
-{'name': 'Colin', 'ai': 10, 'RE': 2, 'SE': 1000, 'CSEC': 1, 'extreme': 5, 'relay': 8, 'security': 1, 'theory': 1, 'sport': 3, 'web': 1000, 'os': 1000, 'debugging': 3, 'mobile': 4, 'gaming': 1000, 'mystery': 5, 'ALL': 0},
-{'name': 'Dan', 'ai': 1000, 'RE': 2, 'SE': 1000, 'CSEC': 4, 'extreme': 6, 'relay': 1000, 'security': 1, 'theory': 1, 'sport': 1, 'web': 6, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
-{'name': 'Gayeon', 'ai': 5, 'RE': 1000, 'SE': 4, 'CSEC': 1000, 'extreme': 1, 'relay': 2, 'security': 1000, 'theory': 1000, 'sport': 1000, 'web': 2, 'os': 1000, 'debugging': 1000, 'mobile': 6, 'gaming': 6, 'mystery': 6, 'ALL': 0},
-{'name': 'Grace', 'ai': 7, 'RE': 1, 'SE': 3, 'CSEC': 2, 'extreme': 5, 'relay': 3, 'security': 1, 'theory': 1, 'sport': 10, 'web': 2, 'os': 6, 'debugging': 5, 'mobile': 5, 'gaming': 6, 'mystery': 5, 'ALL': 0},
-{'name': 'Hassler', 'ai': 2, 'RE': 1000, 'SE': 1000, 'CSEC': 1000, 'extreme': 5, 'relay': 1, 'security': 1000, 'theory': 1000, 'sport': 6, 'web': 3, 'os': 1000, 'debugging': 5, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
-{'name': 'Jack', 'ai': 0, 'RE': 2, 'SE': 9, 'CSEC': 6, 'extreme': 11, 'relay': 4, 'security': 3, 'theory': 3, 'sport': 10, 'web': 13, 'os': 7, 'debugging': 5, 'mobile': 12, 'gaming': 10, 'mystery': 8, 'ALL': 0},
-{'name': 'Jackson', 'ai': 2, 'RE': 1000, 'SE': 4, 'CSEC': 1000, 'extreme': 3, 'relay': 1000, 'security': 1000, 'theory': 1000, 'sport': 1, 'web': 1000, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 1000, 'mystery': 5, 'ALL': 0},
-{'name': 'Jacob', 'ai': 2, 'RE': 4, 'SE': 2, 'CSEC': 1, 'extreme': 1, 'relay': 1, 'security': 1, 'theory': 1, 'sport': 1000, 'web': 3, 'os': 3, 'debugging': 4, 'mobile': 2, 'gaming': 5, 'mystery': 2, 'ALL': 0},
-{'name': 'Joel', 'ai': 2, 'RE': 2, 'SE': 10, 'CSEC': 1000, 'extreme': 5, 'relay': 4, 'security': 2, 'theory': 2, 'sport': 5, 'web': 1000, 'os': 1000, 'debugging': 2, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
-{'name': 'Johnny', 'ai': 1, 'RE': 20, 'SE': 1, 'CSEC': 1000, 'extreme': 5, 'relay': 4, 'security': 20, 'theory': 20, 'sport': 1000, 'web': 3, 'os': 1000, 'debugging': 20, 'mobile': 1000, 'gaming': 1000, 'mystery': 3, 'ALL': 0},
-{'name': 'Lia', 'ai': 5, 'RE': 20, 'SE': 6, 'CSEC': 1000, 'extreme': 1, 'relay': 6, 'security': 1000, 'theory': 1000, 'sport': 5, 'web': 8, 'os': 1000, 'debugging': 1000, 'mobile': 6, 'gaming': 3, 'mystery': 1000, 'ALL': 0},
-{'name': 'Maria', 'ai': 1000, 'RE': 1000, 'SE': 1000, 'CSEC': 1000, 'extreme': 3, 'relay': 1000, 'security': 6, 'theory': 6, 'sport': 1, 'web': 7, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
-{'name': 'Mikayla', 'ai': 5, 'RE': 1000, 'SE': 3, 'CSEC': 1000, 'extreme': 1, 'relay': 6, 'security': 1000, 'theory': 1000, 'sport': 2, 'web': 1000, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 4, 'mystery': 7, 'ALL': 0},
-{'name': 'Nathan', 'ai': 1, 'RE': 5, 'SE': 2, 'CSEC': 1000, 'extreme': 3, 'relay': 8, 'security': 4, 'theory': 4, 'sport': 3, 'web': 7, 'os': 1000, 'debugging': 4, 'mobile': 1000, 'gaming': 1000, 'mystery': 7, 'ALL': 0},
-{'name': 'Ned', 'ai': 3, 'RE': 2, 'SE': 5, 'CSEC': 1000, 'extreme': 6, 'relay': 4, 'security': 10, 'theory': 10, 'sport': 7, 'web': 8, 'os': 1000, 'debugging': 1, 'mobile': 1000, 'gaming': 1000, 'mystery': 9, 'ALL': 0},
-{'name': 'Shir', 'ai': 3, 'RE': 1000, 'SE': 5, 'CSEC': 1000, 'extreme': 2, 'relay': 4, 'security': 1000, 'theory': 1000, 'sport': 1, 'web': 1000, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 1000, 'mystery': 1000, 'ALL': 0},
-{'name': 'Zach', 'ai': 6, 'RE': 7, 'SE': 1000, 'CSEC': 1, 'extreme': 2, 'relay': 1000, 'security': 3, 'theory': 3, 'sport': 1000, 'web': 1000, 'os': 1000, 'debugging': 1000, 'mobile': 1000, 'gaming': 4, 'mystery': 5, 'ALL': 0},
+{'name': 'Jackson', 'os': 13, 'embedded': 9, 'relay': 5, 'sport': 7, 'ai': 6, 'functional': 10, 'ml': 8, 'security': 15, 'swe': 3, 'theory': 1, 'mirego': 12, 'bell': 11, 'olympus': 4, 'cse-cts': 2, 'ALL': 0},
+{'name': 'Gianna', 'os': 8, 'embedded': 9, 'relay': 12, 'sport': 1, 'ai': 7, 'functional': 14, 'ml': 6, 'security': 13, 'swe': 2, 'theory': 3, 'mirego': 10, 'bell': 4, 'olympus': 5, 'cse-cts': 11, 'ALL': 0},
+{'name': 'Ben', 'os': 11, 'embedded': 10, 'relay': 3, 'sport': 2, 'ai': 7, 'functional': 14, 'ml': 1, 'security': 13, 'swe': 4, 'theory': 6, 'mirego': 9, 'bell': 5, 'olympus': 8, 'cse-cts': 12, 'ALL': 0},
+{'name': 'Brandon', 'os': 13, 'embedded': 14, 'relay': 4, 'sport': 7, 'ai': 2, 'functional': 1, 'ml': 5, 'security': 10, 'swe': 8, 'theory': 3, 'mirego': 12, 'bell': 6, 'olympus': 9, 'cse-cts': 11, 'ALL': 0},
+{'name': 'Alan', 'os': 5, 'embedded': 10, 'relay': 14, 'sport': 7, 'ai': 1, 'functional': 6, 'ml': 3, 'security': 12, 'swe': 4, 'theory': 8, 'mirego': 13, 'bell': 9, 'olympus': 2, 'cse-cts': 11, 'ALL': 0},
+{'name': 'Nathan', 'os': 11, 'embedded': 13, 'relay': 4, 'sport': 5, 'ai': 9, 'functional': 7, 'ml': 12, 'security': 8, 'swe': 2, 'theory': 14, 'mirego': 1, 'bell': 3, 'olympus': 6, 'cse-cts': 10, 'ALL': 0},
+{'name': 'Daniel', 'os': 11, 'embedded': 12, 'relay': 9, 'sport': 5, 'ai': 4, 'functional': 13, 'ml': 10, 'security': 14, 'swe': 1, 'theory': 3, 'mirego': 8, 'bell': 2, 'olympus': 6, 'cse-cts': 7, 'ALL': 0},
+{'name': 'Colin', 'os': 5, 'embedded': 7, 'relay': 8, 'sport': 11, 'ai': 10, 'functional': 2, 'ml': 12, 'security': 1, 'swe': 13, 'theory': 6, 'mirego': 14, 'bell': 9, 'olympus': 4, 'cse-cts': 3, 'ALL': 0},
+{'name': 'Shir', 'os': 14, 'embedded': 4, 'relay': 5, 'sport': 9, 'ai': 10, 'functional': 3, 'ml': 7, 'security': 12, 'swe': 11, 'theory': 1, 'mirego': 13, 'bell': 8, 'olympus': 6, 'cse-cts': 2, 'ALL': 0},
+{'name': 'Xiaoya', 'os': 7, 'embedded': 9, 'relay': 4, 'sport': 14, 'ai': 1, 'functional': 10, 'ml': 5, 'security': 11, 'swe': 2, 'theory': 3, 'mirego': 6, 'bell': 8, 'olympus': 13, 'cse-cts': 12, 'ALL': 0},
+{'name': 'Shengbo', 'os': 5, 'embedded': 12, 'relay': 8, 'sport': 14, 'ai': 3, 'functional': 9, 'ml': 2, 'security': 11, 'swe': 1, 'theory': 6, 'mirego': 4, 'bell': 7, 'olympus': 10, 'cse-cts': 13, 'ALL': 0},
+{'name': 'Will', 'os': 12, 'embedded': 3, 'relay': 3, 'sport': 5, 'ai': 3, 'functional': 9, 'ml': 4, 'security': 10, 'swe': 4, 'theory': 1, 'mirego': 5, 'bell': 6, 'olympus': 6, 'cse-cts': 6, 'ALL': 0},
+{'name': 'Shuyang', 'os': 3, 'embedded': 2, 'relay': 8, 'sport': 14, 'ai': 12, 'functional': 7, 'ml': 11, 'security': 3, 'swe': 5, 'theory': 10, 'mirego': 9, 'bell': 4, 'olympus': 3, 'cse-cts': 6, 'ALL': 0},
+{'name': 'Pandrew', 'os': 7, 'embedded': 3, 'relay': 4, 'sport': 13, 'ai': 2, 'functional': 9, 'ml': 1, 'security': 12, 'swe': 5, 'theory': 6, 'mirego': 5, 'bell': 8, 'olympus': 11, 'cse-cts': 10, 'ALL': 0},
+{'name': 'Jason', 'os': 13, 'embedded': 14, 'relay': 2, 'sport': 7, 'ai': 9, 'functional': 3, 'ml': 8, 'security': 10, 'swe': 1, 'theory': 12, 'mirego': 11, 'bell': 5, 'olympus': 4, 'cse-cts': 6, 'ALL': 0},
+{'name': 'Yutong', 'os': 12, 'embedded': 8, 'relay': 5, 'sport': 14, 'ai': 3, 'functional': 13, 'ml': 1, 'security': 11, 'swe': 4, 'theory': 6, 'mirego': 9, 'bell': 2, 'olympus': 10, 'cse-cts': 7, 'ALL': 0},
+{'name': 'Yujie', 'os': 10, 'embedded': 13, 'relay': 12, 'sport': 11, 'ai': 5, 'functional': 4, 'ml': 3, 'security': 15, 'swe': 2, 'theory': 1, 'mirego': 9, 'bell': 6, 'olympus': 7, 'cse-cts': 8, 'ALL': 0},
+{'name': 'Zach', 'os': 8, 'embedded': 9, 'relay': 2, 'sport': 6, 'ai': 4, 'functional': 12, 'ml': 10, 'security': 11, 'swe': 3, 'theory': 14, 'mirego': 13, 'bell': 7, 'olympus': 5, 'cse-cts': 1, 'ALL': 0},
+{'name': 'Michaela', 'os': 12, 'embedded': 8, 'relay': 5, 'sport': 3, 'ai': 10, 'functional': 9, 'ml': 13, 'security': 14, 'swe': 4, 'theory': 2, 'mirego': 11, 'bell': 1, 'olympus': 6, 'cse-cts': 7, 'ALL': 0},
+{'name': 'Joel', 'os': 3, 'embedded': 2, 'relay': 4, 'sport': 10, 'ai': 5, 'functional': 1, 'ml': 10, 'security': 3, 'swe': 14, 'theory': 6, 'mirego': 14, 'bell': 14, 'olympus': 3, 'cse-cts': 5, 'ALL': 0},
 ]
 
 CATEGORIES = [
     {
-        'name': 'ai',
-        'people': 3,
-        'time': [1, 2]
-    },
-    {
-        'name': 'RE',
-        'people': 3,
-        'time': [1]
-    },
-    {
-        'name': 'SE',
-        'people': 3,
-        'time': [1]
-    },
-    {
-        'name': 'CSEC',
+        'name': 'os',
         'people': 2,
-        'time': [2]
+        'time': [5]
     },
     {
-        'name': 'extreme',
+        'name': 'embedded',
         'people': 2,
-        'time': [2]
+        'time': [4]
     },
     {
         'name': 'relay',
@@ -59,49 +44,59 @@ CATEGORIES = [
         'time': [3]
     },
     {
+        'name': 'sport',
+        'people': 2,
+        'time': [4]
+    },
+    {
+        'name': 'ai',
+        'people': 2,
+        'time': [1, 2]
+    },
+    {
+        'name': 'functional',
+        'people': 2,
+        'time': [4]
+    },
+    {
+        'name': 'ml',
+        'people': 2,
+        'time': [5]
+    },
+    {
         'name': 'security',
         'people': 2,
-        'time': [3]
+        'time': [5]
+    },
+    {
+        'name': 'swe',
+        'people': 3,
+        'time': [2, 3]
     },
     {
         'name': 'theory',
         'people': 2,
-        'time': [3]
+        'time': [1],
     },
     {
-        'name': 'sport',
-        'people': 3,
-        'time': [4]
-    },
-    {
-        'name': 'web',
+        'name': 'mirego',
         'people': 2,
         'time': [4],
     },
     {
-        'name': 'os',
+        'name': 'bell',
         'people': 2,
-        'time': [4],
+        'time': [2],
     },
     {
-        'name': 'debugging',
+        'name': 'olympus',
         'people': 2,
-        'time': [5],
+        'time': [1],
     },
     {
-        'name': 'mobile',
+        'name': 'cse-cts',
         'people': 2,
-        'time': [5],
-    },
-    {
-        'name': 'gaming',
-        'people': 2,
-        'time': [5],
-    },
-    {
-        'name': 'mystery',
-        'people': 2,
-        'time': [6],
+        'time': [2, 3],
     },
     {
         'name': 'ALL',
@@ -148,11 +143,7 @@ def str_eq(eq, rel, rhs):
     return ' '.join(map(str, eq)) + " " + rel + " " + str(rhs) + "\n"
 
 def get_weight(w):
-    if w == 1:
-        return 1
-    if w >= 10:
-        return -1
-    return w + 1
+    return w * w
 
 def main():
     vars = []
